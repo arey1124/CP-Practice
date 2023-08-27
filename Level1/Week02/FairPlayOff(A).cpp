657401a38980665c39f53bb761497af9e13f4b15
Arihant <arihant0996@gmail.com>
2023-08-27 12:01:45 -0300

    TLE Eliminators Week 02

--- Level1/Week02/FairPlayOff(A).cpp
+++ Level1/Week02/FairPlayOff(A).cpp
@@ -13,6 +13,13 @@ signed main() {
 
     int t; cin >> t;
     while(t--) {
-        
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if((max(a, b) < min(c, d)) ||  (min(a, b) > max(c, d))) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
     }
 }
---
+++ Level1/Week02/SystemOfEquations(A).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int m,n; cin >> n >> m;
+    
+}
